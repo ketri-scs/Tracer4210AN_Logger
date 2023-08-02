cf4206c0987736a578d52178e57061ae7cdd482f
ETboard <106308185+etboard@users.noreply.github.com>
2023-08-02 20:45:40 +0900

    Configuration File is json

--- src/Config.py
+++ src/Config.py
@@ -7,6 +7,8 @@
 # Modified     :
 # ******************************************************************************************
 
+import json
+
 # Device ID list
 device_id_list = [1, 2]
 
@@ -15,6 +17,18 @@ serial_port = 'COM6'
 
 # Logging Interval (Second Unit)
 logging_interval = 10
+
+# Load from json
+file_path = "./Config.json"
+
+# json 파일 읽어 오기
+with open(file_path, 'r') as file:
+    data = json.load(file)
+    print(data)
+    string_device_id_list = data['device_id_list'].split(',')
+    device_id_list = [int(i) for i in string_device_id_list]
+    serial_port = data['serial_port']
+    logging_interval = data['logging_interval']
 
 # ******************************************************************************************
 # End of File
